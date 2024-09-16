1589429e999257b36958b21086e81e0fd91bc87d
ketanhustles <ketanhustles@gmail.com>
2024-09-17 00:39:04 +0530

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import Demo from './demo';
+import './App.css'
+
+function App() {
+  let a=5;
+  
+
+  return (
+    <>
+      <h1>hello everyone!</h1>
+      <p>this is my variable a:{a}</p>
+      <Demo/>
+
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/demo.jsx
@@ -0,0 +1,6 @@
+const Demo = ()=>{
+    return <>
+        <h2>Demo function</h2>
+    </>
+}
+export default Demo
